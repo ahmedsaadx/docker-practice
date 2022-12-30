c68bcf10303790a512f3a8f07cd42857cefcebd7
ahmedsaadx <ahmeddsaadd188@gmail.com>
2022-12-30 07:56:50 +0200

    tasks

--- projects/project1/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const PORT = 4040;
-const app = express();
-
-app.get('/',(req, res) => res.send('<h1> hello world<h1>'));
-
-app.listen(PORT, () => console.log(`app is up and running on port ${PORT}`));
